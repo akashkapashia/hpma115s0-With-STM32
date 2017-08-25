e4f4350378d36f012ed6ec980a299c160f31b4f8
akash kapashia <akashkapashia@gmail.com>
2017-08-25 12:55:21 +0530

    Add files via upload

---
+++ Honeywell_DustSensor.h
@@ -0,0 +1,27 @@
+/**
+	******************************************************************************
+	* File Name          : Honeywell_DustSensor.H
+	* Description        : This file contains the common defines of the application
+	******************************************************************************
+	*
+	* COPYRIGHT(c) 2017 Akash kapashia
+       * Created by Akash kapashia
+	******************************************************************************
+*/
+ 
+/* Define to prevent recursive inclusion -------------------------------------*/
+
+
+ #ifndef __HONEYWELL_DUSTSENSOR_H
+ #define __HONEYWELL_DUSTSENSOR_H
+	 
+#define USART_DUSTSENSOR USART1
+
+ void start_Particle_Measure(void);	
+ void MX_USART_DUSTSENSOR_UART_Init(void);
+ void Stop_Particle_Measurement(void);
+ void Read_Particle_Measuring(void);
+ void Enable_Auto_Send(void);
+ void Stop_Auto_Send(void);
+
+#endif
